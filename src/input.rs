7b711ce9541cd09f8bf851e3f49d83be9b7e6fe9
Marshall Bowers <elliott.codes@gmail.com>
2018-12-01 12:42:33 -0500

    Add some documentation

--- src/input.rs
+++ src/input.rs
@@ -2,15 +2,19 @@ use std::fs::File;
 use std::io::Read;
 use std::path::Path;
 
+/// The input for an Advent of Code puzzle.
 pub struct Input {
     pub value: String,
 }
 
 impl Input {
+    /// Returns a new [`Input`] from the given [`String`].
     pub fn new(value: String) -> Self {
         Self { value }
     }
 
+    /// Returns a new [`Input`] containing the contents of the file at the
+    /// given path.
     pub fn from_file<P: AsRef<Path>>(path: P) -> std::io::Result<Self> {
         let mut file = File::open(path)?;
         let mut contents = String::new();
