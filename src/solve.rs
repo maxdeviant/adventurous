81458930e0265fe7e2d88166a1e4ee839c93fec3
Marshall Bowers <elliott.codes@gmail.com>
2023-12-02 15:23:28 -0500

    Update `Solve` docs

--- src/solve.rs
+++ src/solve.rs
@@ -7,7 +7,7 @@ use crate::Input;
 /// A trait for solving an Advent of Code puzzle.
 ///
 /// This trait is automatically implemented for functions that take a reference to
-/// an [`Input`] and return a [`Result`] containing a type that implements [`Display`].
+/// an [`Input`] and return a [`Result`] containing a type that implements [`Display`]:
 ///
 /// ```
 /// use adventurous::Input;
