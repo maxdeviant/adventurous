8939e133a3382fd4b343c9e06619e9a7d3f035b2
Marshall Bowers <elliott.codes@gmail.com>
2023-12-02 22:53:32 -0500

    Ignore solution tests that don't yet have an answer

--- crates/adventurous_macros/src/part.rs
+++ crates/adventurous_macros/src/part.rs
@@ -1,7 +1,12 @@
+use std::sync::OnceLock;
+
 use proc_macro2::TokenStream;
 use quote::quote;
 use syn::parse::Parse;
 use syn::Token;
+
+pub(crate) static PART_ONE_ANSWER: OnceLock<Option<String>> = OnceLock::new();
+pub(crate) static PART_TWO_ANSWER: OnceLock<Option<String>> = OnceLock::new();
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
 pub enum Part {
@@ -10,6 +15,16 @@ pub enum Part {
 }
 
 impl Part {
+    pub fn part_fn_ident(&self) -> syn::Ident {
+        let part_number = match self {
+            Part::One => "one",
+            Part::Two => "two",
+        };
+
+        syn::parse_str::<syn::Ident>(&format!("part_{part_number}"))
+            .expect("failed to create identifier")
+    }
+
     pub fn answer_fn_ident(&self) -> syn::Ident {
         let part_number = match self {
             Part::One => "one",
@@ -51,6 +66,11 @@ impl Parse for PartInput {
 }
 
 pub fn part_impl(part: Part, input: PartInput, item: TokenStream) -> TokenStream {
+    match part {
+        Part::One => PART_ONE_ANSWER.get_or_init(|| input.answer.clone()),
+        Part::Two => PART_TWO_ANSWER.get_or_init(|| input.answer.clone()),
+    };
+
     let answer_fn_ident = part.answer_fn_ident();
 
     let answer = match input.answer {
--- crates/adventurous_macros/src/test_solutions.rs
+++ crates/adventurous_macros/src/test_solutions.rs
@@ -1,32 +1,49 @@
 use proc_macro2::TokenStream;
 use quote::quote;
 
-use crate::part::Part;
+use crate::part::{Part, PART_ONE_ANSWER, PART_TWO_ANSWER};
 
 pub fn test_solutions_impl() -> TokenStream {
-    let part_one_answer_fn_ident = Part::One.answer_fn_ident();
-    let part_two_answer_fn_ident = Part::Two.answer_fn_ident();
+    let part_one_test = test_solution(Part::One);
+    let part_two_test = test_solution(Part::Two);
 
     quote! {
-        #[test]
-        fn test_part_one_solution() -> anyhow::Result<()> {
-            match #part_one_answer_fn_ident() {
-                Some(answer) => adventurous::test::test_solution("input.txt", part_one, answer),
-                None => {
-                    eprintln!("Part One not solved");
-                    Ok(())
+        #part_one_test
+        #part_two_test
+    }
+}
+
+fn test_solution(part: Part) -> TokenStream {
+    let answer = match part {
+        Part::One => PART_ONE_ANSWER.get().and_then(|answer| answer.clone()),
+        Part::Two => PART_TWO_ANSWER.get().and_then(|answer| answer.clone()),
+    };
+
+    let part_fn = part.part_fn_ident();
+    let test_fn_ident = {
+        let part_number = match part {
+            Part::One => "one",
+            Part::Two => "two",
+        };
+
+        syn::parse_str::<syn::Ident>(&format!("test_part_{part_number}_solution"))
+            .expect("failed to create identifier")
+    };
+
+    match answer {
+        Some(answer) => {
+            quote! {
+                #[test]
+                fn #test_fn_ident() -> anyhow::Result<()> {
+                    adventurous::test::test_solution("input.txt", #part_fn, #answer)
                 }
             }
         }
-
-        #[test]
-        fn test_part_two_solution() -> anyhow::Result<()> {
-            match #part_two_answer_fn_ident() {
-                Some(answer) => adventurous::test::test_solution("input.txt", part_two, answer),
-                None => {
-                    eprintln!("Part Two not solved");
-                    Ok(())
-                }
+        None => {
+            quote! {
+                #[test]
+                #[ignore = "not yet solved"]
+                fn #test_fn_ident() {}
             }
         }
     }
